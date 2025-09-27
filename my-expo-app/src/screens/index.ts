7d974043a54d0b1f49e44553324d9632cafb57ed
Hiran Thathsara <hiranthathsara9@gmail.com>
2025-09-27 13:33:59 +0530

    Update screen exports in index.ts: replace DashboardScreen with HomeScreen and add additional screens

--- my-expo-app/src/screens/index.ts
+++ my-expo-app/src/screens/index.ts
@@ -4,7 +4,7 @@ export { default as WelcomeScreen } from './WelcomeScreen';
 export { default as RoleSelectionScreen } from './RoleSelectionScreen';
 export { default as SignUpScreen } from './SignUpScreen';
 export { default as SignInScreen } from './SignInScreen';
-export { default as DashboardScreen } from './DashboardScreen';
+export { default as HomeScreen } from './HomeScreen';
 
 // New screens
 export { default as EventsScreen } from './EventsScreen';
@@ -14,3 +14,9 @@ export { default as BusinessRegistrationStep1 } from './BusinessRegistrationStep
 export { default as BusinessRegistrationStep2 } from './BusinessRegistrationStep2';
 export { default as BusinessRegistrationStep3 } from './BusinessRegistrationStep3';
 export { default as BusinessRegistrationStep4 } from './BusinessRegistrationStep4';
+export { default as DirectoryScreen } from './DirectoryScreen';
+export { default as EcoRewardsScreen } from './EcoRewardsScreen';
+export { default as NotificationsScreen } from './NotificationsScreen';
+export { default as ProfileScreen } from './ProfileScreen';
+export { default as HotelDetailScreen } from './HotelDetailScreen';
+export { default as ReviewsScreen } from './ReviewsScreen';
